80ccec5b4558fb5c783ce2d6d98691ebf369e1d6
Chi Joel <abongsjoel@gmail.com>
2022-07-28 15:18:35 +0100

    Display hard coded stories

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Stories from "./components/Stories";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello and Welcome</h1>
+      <h3>My Favorite Stories on Medium Are:</h3>
+      <Stories />
     </div>
   );
 }
